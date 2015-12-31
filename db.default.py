2ff5daf480c8f2f3af56338a9fa2d59bb0a31405
imlonghao <shield@fastmail.com>
2015-12-31 15:31:36 +0800

    Set up the DB file

---
+++ db.default.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import motor
+
+USERNAME = ''
+PASSWORD = ''
+HOST = ''
+PORT = 27017
+DATABASE = ''
+
+
+def db():
+    return motor.MotorClient('mongodb://%s:%s@%s:%s/%s' % (
+        USERNAME, PASSWORD, str(PORT),
+        HOST, DATABASE
+    )).DATABASE
